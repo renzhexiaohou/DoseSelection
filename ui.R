ee546d02092b23f6e8c27e8588a6aca131506326
Yubo Xiao <xiaoyubocpu@163.com>
2020-09-09 13:28:59 +0800

    Update ui.R

--- ui.R
+++ ui.R
@@ -1,8 +1,6 @@
 # Author: Yubo Xiao
-# Date: 11-Jul-2019 
+# Date: 09-Sep-2020 
 # options(warn = -1)
-# suppressWarnings(library(shiny))
-# suppressWarnings(library(shinythemes))
 
 library(shiny)
 library(shinythemes)
@@ -18,7 +16,7 @@ shinyUI(
       
       # second page: simulation -------------------------------------------------
       # tabPanel("模拟预测",
-      tabPanel(strong("for 50% or 90% population ?"),
+      tabPanel(strong("50% or 90% population ?"),
                #左栏
                fluidRow(
                  column(4,
@@ -30,8 +28,8 @@ shinyUI(
                         wellPanel(
                           style = "background: #ededed",
                           fluidRow(
-                            column(4, numericInput("amt", h4(strong("Dose (mg)")), value = 120)),
-                            column(4, numericInput("interval", h4(strong("Dose interval (h)")), value = 24)),
+                            column(4, numericInput("amt", h4(strong("Dose (mg)")), value = 30)),
+                            column(4, numericInput("tau", h4(strong("Dose interval (h)")), value = 24)),
                             column(4, numericInput("n", h4(strong("Number of doses")), value = 24)),
                             tags$div(id = 'placeholder')
                           ),
@@ -39,8 +37,8 @@ shinyUI(
                           # titlePanel(h5(strong("浓度范围 (mg/L)"))),
                           titlePanel(h4(strong("Observed time range (h)"))),
                           sliderInput("obs", label = NULL, min = 0, max = 168, step = 12, value = c(0,168)),
-                          titlePanel(h4(strong("Theraputic concentration range (mg/L)"))),
-                          sliderInput("mec", label = NULL, min = 0, max = 10, step = 1, value = c(1, 3))
+                          titlePanel(h4(strong("Theraputic concentration range (ng/mL)"))),
+                          sliderInput("mec", label = NULL, min = 0, max = 1000, step = 50, value = c(100, 600))
                         ),
                         # 模型参数的设定 ----------------------------------------
                         # titlePanel(h3(strong("模型参数"))),
@@ -48,19 +46,22 @@ shinyUI(
                         wellPanel(
                           style = "background: #ededed",
                           fluidRow(
-                            column(6,numericInput("cl", "CL (L/h)", value = 2.7)),
-                            column(6,numericInput("v", "V (L)", value = 100))
+                            column(4,numericInput("cl", "CL (L/h)", value = 7)),
+                            column(4,numericInput("v2", "Vc (L)", value = 30)),
+                            column(4,numericInput("v3", "Vp (L)", value = 100))
                           ),
                           fluidRow(
-                            column(6,numericInput("ka", "Ka (/h)", value = 0.8)),
-                            column(6,numericInput("f", "F ", value = 0.8))
+                            column(4,numericInput("q", "Q (L/h)", value = 30)),
+                            column(4,numericInput("ka", "Ka (/h)", value = 0.6)),
+                            column(4,numericInput("fr", "F ", value = 0.8))
                           ),
                           fluidRow(
-                            column(4, numericInput("omega1", "BSV_CL", value = 0.2)),
-                            column(4, numericInput("omega2", "BSV_V", value = 0.3)),
-                            column(4, numericInput("omega3", "BSV_F", value = 0.3))
+                            column(4, numericInput("bsv_cl", "BSV_CL", value = 0.2)),
+                            column(4, numericInput("bsv_v2", "BSV_Vc", value = 0.3)),
+                            # column(4, numericInput("bsv_v3", "BSV_Vp", value = 0.3)),
+                            column(4, numericInput("bsv_ka", "BSV_Ka", value = 0.3))
                           ),
-                          h6("Notes: CL, clearance; V, volume of distribution; Ka, absorption constant; F, bioavailability; BSV, between subject variability.")
+                          h6("Notes: CL, clearance; Vc, central volume of distribution; Vp, peripheral volume of distribution; Q, inter-compartmental clearance; Ka, absorption constant; F, bioavailability; BSV, between subject variability.")
                         )
                  ),
                  column(8,
@@ -70,34 +71,15 @@ shinyUI(
                         br(),
                         br(),
                         br(),
-                        # absolutePanel(
-                        #   top = 6, right = 580, width = 160, height = 10, draggable = FALSE,
-                        #   checkboxGroupInput("res", "",
-                        #                      choices = c("Average", "Pooled"),
-                        #                      selected = "Average", inline = TRUE)
-                        # ),
-                        # conditionalPanel(condition = "input.res == 'Average'",
-                        #                  plotOutput(outputId = "idvpkplot")),
-                        # conditionalPanel(condition = "input.res == 'Pooled'",
-                        #                  plotOutput(outputId = "poppkplot"))
                         plotOutput(outputId = "idvpkplot"),
                         plotOutput(outputId = "poppkplot")
                  )
                ),
                
-               # absolutePanel(
-               #   top = 160, right = 295, width = 335, height = 10, draggable = FALSE,
-               #   titlePanel(h4(strong(textOutput(outputId = "proportion1", inline = T))))
-               # ),
                absolutePanel(
                  top = 170, right = 60, width = 100, height = 10, draggable = TRUE,
                  img(src="LOGOdMed.png", height = 50)
                ),
-               
-               # absolutePanel(
-               #   top = 560, right = 295, width = 335, height = 10, draggable = FALSE,
-               #   titlePanel(h4(strong(textOutput(outputId = "proportion2", inline = T))))
-               # ),
                absolutePanel(
                  top = 570, right = 60, width = 100, height = 10, draggable = TRUE,
                  img(src="LOGOdMed.png", height = 50)
@@ -109,7 +91,7 @@ shinyUI(
                ),
                absolutePanel(
                  top = 90, right = 370, width = 160, height = 10, draggable = FALSE,
-                 sliderInput("idv", label = NULL, min = 0, max = 500, step = 50, value = 50)
+                 sliderInput("idv", label = NULL, min = 0, max = 1000, step = 10, value = 100)
                ),
                
                absolutePanel(
@@ -120,14 +102,8 @@ shinyUI(
                  top = 90, right = 80, width = 160, height = 10, draggable = FALSE,
                  sliderInput("p", label = NULL, min = 0, max = 1, step = 0.05, value = c(0.05,0.95))
                )
-               # ,
-               # absolutePanel(
-               #   top = 90, left = 280, width = 300, height = 10, draggable = FALSE,
-               #   img(src="LOGOdMed.png", height = 50)
-               # )
       ),
-      # img(src="2sdh.png",height = 55, width = 225)
-      # <code style='color:#ec4c3c;background-color:#F8F9F9'>
+      
       footer = h5(HTML("dMed Copyright 2020 : 
                        <strong style='color:#ec4c3c;background-color:#F8F9F9'> E </strong>
                        arly <strong style='color:#ec4c3c;background-color:#F8F9F9'> D </strong> evelepment and 
